eabd44388797a6f74a6377e429ad3eef1a592f0c
Sam Verschueren <sam.verschueren@gmail.com>
2018-10-20 09:34:19 +0200

    Fix and upgrade dependencies

--- server/src/package.ts
+++ server/src/package.ts
@@ -1,6 +1,15 @@
 import * as path from 'path';
 import * as loadJsonFile from 'load-json-file';
 
+interface PackageJSON {
+	dependencies?: {
+		[key: string]: string;
+	};
+	devDependencies?: {
+		[key: string]: string;
+	};
+}
+
 export class Package {
 	constructor(
 		private readonly workspaceRoot: string
@@ -8,7 +17,7 @@ export class Package {
 
 	isDependency(name: string) {
 		try {
-			const pkg = loadJsonFile.sync(path.join(this.workspaceRoot, 'package.json'));
+			const pkg = loadJsonFile.sync(path.join(this.workspaceRoot, 'package.json')) as PackageJSON;
 			const deps = pkg.dependencies || {};
 			const devDeps = pkg.devDependencies || {};
 
